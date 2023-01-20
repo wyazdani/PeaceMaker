8a227d09617053454750d576cd9ab98d23ca5806
rixwanmirza <90381042+rixwanmirza@users.noreply.github.com>
2023-01-20 17:51:25 +0500

    code commit 20JanFri600pm

--- src/Components/SupportSection/SupportSideBar/index.js
+++ src/Components/SupportSection/SupportSideBar/index.js
@@ -3,41 +3,46 @@ import user1 from "Images/unsplash.png";
 import user2 from "Images/unsplash2.png";
 import user3 from "Images/unsplash3.png";
 import classes from "./index.module.scss";
+import {Link} from "react-router-dom";
 
 const SupportSideBar = () => {
   return (
-    <div className={classes.messageListSidebar}>
+      <div className={classes.messageListSidebar}>
         <h1>Messages</h1>
-    <ul>
-      <li  className={classes.active}>
-        <div className={classes.userImg}>
-          <img src={user1} alt="username" />
-        </div>
-        <div className={classes.description}>
-          <h6>Marcus Curtis</h6>
-         </div>
-      </li>
-      
-      <li>
-        <div className={classes.userImg}>
-          <img src={user2} alt="username" />
-        </div>
-        <div className={classes.description}>
-          <h6>Marcus Curtis</h6>
-          
-       
-        </div>
-      </li>
-      <li>
-        <div className={classes.userImg}>
-          <img src={user3} alt="username" />
-        </div>
-        <div className={classes.description}>
-          <h6>Marcus Curtis</h6>
-        </div>
-      </li>
-    </ul>
-  </div>
+        <ul>
+          <li  className={classes.active}>
+            <Link to={"#"}>
+              <div className={classes.userImg}>
+                <img src={user1} alt="username" />
+              </div>
+              <div className={classes.description}>
+                <h6>Marcus Curtis</h6>
+              </div>
+            </Link>
+          </li>
+
+          <li>
+            <Link to={"#"}>
+              <div className={classes.userImg}>
+                <img src={user2} alt="username" />
+              </div>
+              <div className={classes.description}>
+                <h6>Marcus Curtis</h6>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to={"#"}>
+              <div className={classes.userImg}>
+                <img src={user3} alt="username" />
+              </div>
+              <div className={classes.description}>
+                <h6>Marcus Curtis</h6>
+              </div>
+            </Link>
+          </li>
+        </ul>
+      </div>
   )
 }
 
--- src/Pages/Messages/index.js
+++ src/Pages/Messages/index.js
@@ -1,86 +1,22 @@
-import classes from "./index.module.scss";
-import React from 'react'
-import Col from 'react-bootstrap/Col';
-import Nav from 'react-bootstrap/Nav';
-import Row from 'react-bootstrap/Row';
-import Tab from 'react-bootstrap/Tab';
-import { Button, Card } from "react-bootstrap";
-import SecondHeader from "Components/Header/SecondHeader";
-import Time from "Pages/Affirmations/SettingIconClick/Time";
-import BloodPressure from "Pages/Affirmations/SettingIconClick/BloodPressure";
-import BodyTemperature from "Pages/Affirmations/SettingIconClick/BodyTemperature";
-import profile from '../../Images/unsplash.png'
-import unsplash from '../../Images/unsplash1.png'
-import unsplash1 from '../../Images/unsplash3.png'
-import MessagePopUp from "./PopUp";
+import SecondHeader from "Components/Header/SecondHeader/index.js";
+import PageTitle from "Components/Pagetitle";
+import React from "react";
+import { Card, Row, Tab, Tabs } from "react-bootstrap";
+import EndUserSupport from "./EndUser/index.js";
+import Tickets from "./Ticket.js";
 
-// import Sonnet from '../../components/Sonnet';
-
-function LeftTabsExample(props) {
-    const [modalShow, setModalShow] = React.useState(false);
+const Messages = () => {
     return (
         <>
             <SecondHeader />
-            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-                <section  className={classes.main_tabs}>
-                    <Row>
-                        <Col sm={4} className={classes.border_line} >
-
-                            <h2>Messages</h2>
-                            <Nav variant="pills" className="flex-column vertical-tabs message_tabs">
-                                <Nav.Item>
-                                    <Nav.Link className={classes.nav_styling} eventKey="first">
-                                        <div className="d-flex justify-content-between align-items-center">
-                                            <div className={classes.icon_text}>
-                                                <img src={profile} alt="" />
-                                                <p><strong>Thomas Edison</strong> <br /> Thanks! I’ll get back to you...</p>
-                                            </div>
-                                            <time> 1:23 PM </time>
-                                        </div>
-                                    </Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link className={classes.nav_styling} eventKey="second">
-                                        <div className="d-flex justify-content-between">
-                                            <div className={classes.icon_text}>
-                                                <img src={unsplash} alt="" />
-                                                <p><strong>Thomas Edison</strong> <br /> Thanks! I’ll get back to you...</p>
-                                            </div>
-                                            <time> 1:23 PM </time>
-                                        </div>
-                                    </Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link className={classes.nav_styling} eventKey="third">
-                                        <div className="d-flex justify-content-between">
-                                            <div className={classes.icon_text}>
-                                                <img src={unsplash1} alt="" />
-                                                <p><strong>Thomas Edison</strong> <br /> Thanks! I’ll get back to you...</p>
-                                            </div>
-                                            <time> 1:23 PM </time>
-                                        </div>
-                                    </Nav.Link>
-                                </Nav.Item>
-                            </Nav>
-                            <div className={classes.btn_bottom}>
-                                <button className='btn btn-gradient d-block w-50'>Add Habit</button>
-                            </div>
-                        </Col>
-
-                        <Col  onClick={() => setModalShow(true)}  sm={8} className='position-relative'>
-                         
-
-                        </Col>
-
-                    </Row>
-                </section>
-            </Tab.Container>
-            <MessagePopUp
-            show={modalShow}
-            onHide={() => setModalShow(false)}
-        />
+            <section>
+                <Card className="">
+                    <Tickets/>
+                    {/* <EndUserSupport/> */}
+                </Card>
+            </section>
         </>
     );
-}
+};
 
-export default LeftTabsExample;
+export default Messages;
