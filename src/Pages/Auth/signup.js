5d9623ba3bc87211e78107cac4f1b678dc902ce5
sa-Wahab <wahab@softwarealliance.io>
2023-03-03 12:02:10 +0500

    my first commit

--- src/Pages/Auth/signup.js
+++ src/Pages/Auth/signup.js
@@ -25,6 +25,7 @@ const SignUp = () => {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(data)
+          
         })
           .then(response => response.json())
           .then(data => console.log(data))
